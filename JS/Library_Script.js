c702c4b3bd53db0212ef6fd04c04afe9c54ca187
ARLIKIN <nik5552000@mail.ru>
2023-07-19 18:29:18 +0400

    The library has been completed

--- JS/Library_Script.js
+++ JS/Library_Script.js
@@ -13,15 +13,7 @@ var User_Lib;
 var NameTitle;
 
 
-function InitScreenshot()
-{
-  const $slider = document.querySelector('[data-slider="chiefslider"]');
-    const slider = new ChiefSlider($slider, {
-      loop: false
-    });
-}
-
-
+//инициализация 
 function InitLib()
 {
     User_Lib =  JSON.parse(fs.readFileSync('Lib_user.json','utf8'));
@@ -44,72 +36,110 @@ function InitLib()
     }
 
     NewGameLib(Object.keys(User_Lib)[0]);
-    InitScreenshot()
+    InitScreenshot();
 }
-
+//обработка открытия страницы
 function NewGameLib(title)
 {
     Byid('20Torrents').innerHTML = '';
     Byid('Torrent_fon1').hidden = true;
     Byid('Torrent_fon2').hidden = true;
+    Byid('Setting').textContent = "⋮";
     NameTitle = title
-    try
-    {
-      Byid('HeadIMG').style= "background-image: url('"+User_Lib[title].background_raw+"');" 
-    }catch
-    {
-      Byid('HeadIMG').style= "background-image: url(../img/StandartBG.png);" 
-    }
-    
-    Byid('NameTitle').textContent = title;
-    Byid('release_date').textContent = 'Дата выхода:' + User_Lib[title].release_date;
-    
-    var text = 'Разработчики: ';
-    for(var i =0; i < User_Lib[title].publishers.length; i++)
-    {
-        text += User_Lib[title].publishers[i] + '<br>';
-    }
 
-    Byid('vendor').innerHTML = text;
-
-
-    Byid('PCrecomend_min').innerHTML = User_Lib[title].pc_requirements.minimum;
-    if(User_Lib[title].pc_requirements.recommended)
+    if(User_Lib[title].source == "steam")
     {
-        Byid('PCrecomend_max').innerHTML = User_Lib[title].pc_requirements.recommended;
+      SteamSource();
     }else
     {
-        Byid('PCrecomend_max').innerHTML = '';
+      AnotherSource();
     }
-
-    if(User_Lib[title].path != "")
-    Byid('Setting').textContent = '✓'
-
-    //Видео
-    Byid('slider_items').innerHTML = '';
-    var OnePart = '<div class="slider__item"><div class="slider__item-container"><div class="slider__item-content">';
-    var TwoPart = '</div></div></div>';
-    if(User_Lib[title].movies)
-    for(var i =0; i < User_Lib[title].movies.length; i++)
+    // Игра из стима
+    function SteamSource()
     {
-      Byid('slider_items').innerHTML += OnePart +'<video class="video" controls="" name="media" src="'+ User_Lib[title].movies[i].mp4.max +'"></video>'+ TwoPart;
-    }
+          try
+        {
+          Byid('HeadIMG').style= "background-image: url('"+User_Lib[title].background_raw+"');" 
+        }catch
+        {
+          Byid('HeadIMG').style= "background-image: url(../img/BG1.jpg);" 
+        }
+        
+        Byid('NameTitle').textContent = title;
+        Byid('release_date').textContent = 'Дата выхода:' + User_Lib[title].release_date;
+        
+        var text = 'Разработчики: ';
+        for(var i =0; i < User_Lib[title].publishers.length; i++)
+        {
+            text += User_Lib[title].publishers[i] + '<br>';
+        }
 
-    //Скриншоты
-    if(User_Lib[title].screenshots)
-    for(var i =0; i < User_Lib[title].screenshots.length; i++)
-    {
-      Byid('slider_items').innerHTML += OnePart +'<img class="screen" src="'+ User_Lib[title].screenshots[i].path_full+'" alt="">'+ TwoPart;
+        Byid('vendor').innerHTML = text;
+
+
+        Byid('PCrecomend_min').innerHTML = User_Lib[title].pc_requirements.minimum;
+        if(User_Lib[title].pc_requirements.recommended)
+        {
+            Byid('PCrecomend_max').innerHTML = User_Lib[title].pc_requirements.recommended;
+        }else
+        {
+            Byid('PCrecomend_max').innerHTML = '';
+        }
+
+        if(User_Lib[title].path != "")
+        Byid('Setting').textContent = '✓'
+
+        //Видео
+        Byid('slider_items').innerHTML = '';
+        var OnePart = '<div class="slider__item"><div class="slider__item-container"><div class="slider__item-content">';
+        var TwoPart = '</div></div></div>';
+        if(User_Lib[title].movies)
+        for(var i =0; i < User_Lib[title].movies.length; i++)
+        {
+          Byid('slider_items').innerHTML += OnePart +'<video class="video" controls="" name="media" src="'+ User_Lib[title].movies[i].mp4.max +'"></video>'+ TwoPart;
+        }
+
+        //Скриншоты
+        if(User_Lib[title].screenshots)
+        for(var i =0; i < User_Lib[title].screenshots.length; i++)
+        {
+          Byid('slider_items').innerHTML += OnePart +'<img class="screen" src="'+ User_Lib[title].screenshots[i].path_full+'" alt="">'+ TwoPart;
+        }
+        Byid('Content_Video_Rec').hidden = false;
+        Byid('release_date').hidden = false;
+        Byid('vendor').hidden = false;
+        
     }
+    // Добавленная игра
+    function AnotherSource()
+    {
     
-}
+      Byid('HeadIMG').style= "background-image: url(../img/BG1.jpg);" 
+      Byid('NameTitle').textContent = title;
+      if(User_Lib[title].path != "")
+      Byid('Setting').textContent = '✓';
+      Byid('release_date').hidden = true;
+      Byid('vendor').hidden = true;
+      Byid('Content_Video_Rec').hidden = true;
 
+    }
+}
+// Open Page Game
 function OpenLibGame()
 {
     NewGameLib(this.id)
 }
 
+// Слайдер изображений
+function InitScreenshot()
+{
+  const $slider = document.querySelector('[data-slider="chiefslider"]');
+    const slider = new ChiefSlider($slider, {
+      loop: false
+    });
+}
 
+//Кнопка загрузки
 Byid('BTNDowloand').onclick = function()
     {
       var query = Byid('NameTitle').textContent;
@@ -219,7 +249,7 @@ Byid('BTNDowloand').onclick = function()
     }
     }    
 
-
+//Кнопки играть и настройка
 Byid('BTNPlay').onclick = function()
 {
     var path = User_Lib[NameTitle].path;
@@ -252,7 +282,57 @@ Byid('Setting').onclick = function()
     .catch(err => alert(err))
 }
 
+Byid('BTNDelete').onclick = function()
+{
+  delete User_Lib[NameTitle];
+  fs.writeFileSync('Lib_user.json',JSON.stringify(User_Lib));
+  newInit();
+}
 
+function newInit()
+{
+  Byid('Games_lib').innerHTML = '';
+  InitLib();
+}
+
+//Кнопка добавить игру
+Byid('Game_fon_div').onclick = function()
+{
+  var Path;
+  var name;
+ 
+  
+    function Path(dir)
+    {
+      Path = dir;
+      var temp = dir.split('/');
+      name = temp[temp.length-1].split('.')[0];
+      Save();
+    }
+
+    function Save()
+    {
+      User_Lib[name] = {};
+        User_Lib[name]["steam_appid"] =  undefined;
+        User_Lib[name]["header_image"] = "../img/Default_Libary.png";
+        User_Lib[name]["release_date"] = undefined;
+        User_Lib[name]["publishers"] = undefined;
+        User_Lib[name]["pc_requirements"] = undefined;
+        User_Lib[name]["background_raw"] = undefined;
+        User_Lib[name]["movies"] = undefined;
+        User_Lib[name]["screenshots"] = undefined;
+        User_Lib[name]["source"] = "another";
+        User_Lib[name]["path"] = Path;
+        fs.writeFileSync('Lib_user.json',JSON.stringify(User_Lib));
+        newInit();
+    }
+  
+    const config={type:'open-file'}
+    dialog(config)
+      .then(dir => Path(dir[0]))
+      .catch(err => alert(err))
+  
+}
 //✓⋮
 
 
